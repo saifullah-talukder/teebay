361851348f85ede43c4bccd9c35d5956419788c5
Saifullah Talukder <saifullah.csebuet@gmail.com>
2025-02-19 06:17:35 +0000

    move all resolver composition to one file

--- backend/src/providers/GraphQLResolvers.ts
+++ backend/src/providers/GraphQLResolvers.ts
@@ -1,15 +1,33 @@
-import { CategoryTypeResolver } from '../resolvers/CategoryResolver'
-import { MutationResolver } from '../resolvers/MutationResolver'
-import { ProductTypeResolver } from '../resolvers/ProductResolver'
-import { QueryResolver } from '../resolvers/QueryResolver'
-import { RentalTypeResolver } from '../resolvers/RentalResolver'
-import { TransactionTypeResolver } from '../resolvers/TransactionResolver'
-import { UserTypeResolver } from '../resolvers/UserResolver'
+import { AuthResolver } from '../resolvers/AuthResolver'
+import { CategoryQueryResolver, CategoryTypeResolver } from '../resolvers/CategoryResolver'
+import { ProductMutationResolver, ProductQueryResolver, ProductTypeResolver } from '../resolvers/ProductResolver'
+import { RentalMutationResolver, RentalQueryResolver, RentalTypeResolver } from '../resolvers/RentalResolver'
+import {
+  TransactionMutationResolver,
+  TransactionQueryResolver,
+  TransactionTypeResolver,
+} from '../resolvers/TransactionResolver'
+import { UserQueryResolver, UserTypeResolver } from '../resolvers/UserResolver'
 
 export function registerGraphQLResolvers() {
+  const queryResolver = {
+    ...new UserQueryResolver().register(),
+    ...new ProductQueryResolver().register(),
+    ...new TransactionQueryResolver().register(),
+    ...new RentalQueryResolver().register(),
+    ...new CategoryQueryResolver().register(),
+  }
+
+  const mutaionResolver = {
+    ...new AuthResolver().register(),
+    ...new ProductMutationResolver().register(),
+    ...new TransactionMutationResolver().register(),
+    ...new RentalMutationResolver().register(),
+  }
+
   return {
-    Query: new QueryResolver().register(),
-    Mutation: new MutationResolver().register(),
+    Query: queryResolver,
+    Mutation: mutaionResolver,
     User: new UserTypeResolver().register(),
     Product: new ProductTypeResolver().register(),
     Category: new CategoryTypeResolver().register(),
